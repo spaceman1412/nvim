40bc44af12e1a6b094ce75b5de3d6146adeb0fcd
spaceman1412 <spacemannormal@gmail.com>
2024-09-17 16:13:27 +0700

    Config keymap and add prettier

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Resume search telescope
+vim.keymap.set(
+  "n",
+  "<leader>sR",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "Resume Search Telescope" }
+)
