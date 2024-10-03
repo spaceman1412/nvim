931ba321e78447e2633c9d0baa90130a8a4d48c0
spaceman1412 <spacemannormal@gmail.com>
2024-10-03 11:44:50 +0700

    Add obsidian plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,10 +2,93 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local telescope_builtin = require("telescope.builtin")
+local whichkey_builtin = require("which-key")
+
+-- Add group obisidan to which-key
+whichkey_builtin.add({
+  { "<leader>o", group = "obsidian" },
+})
+
 -- Resume search telescope
 vim.keymap.set(
   "n",
   "<leader>sR",
-  require("telescope.builtin").resume,
+  telescope_builtin.resume,
   { noremap = true, silent = true, desc = "Resume Search Telescope" }
 )
+-- Delete file in current buffer
+-- vim.keymap.set("n", "<leader>fd", ":!rm '%:p'<cr>:bd<cr>", {
+--   desc = "Delete file in current buffer",
+-- })
+
+-- #OBSIDIAN
+-- Navigate to vault
+vim.keymap.set("n", "<leader>oo", ":cd /Users/chaileasevn/Documents/Obsidian Vault<cr>", {
+  desc = "Navigate to Obsidian Vault",
+})
+
+-- convert note to template and remove leading white space
+vim.keymap.set("n", "<leader>oc", ":ObsidianTemplate note<cr> :lua vim.cmd([[1,/^\\S/s/^\\n\\{1,}//]])<cr>", {
+  desc = "Convert note to template",
+})
+-- strip date from note title and replace dashes with spaces
+-- must have cursor on title
+vim.keymap.set("n", "<leader>of", ":s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<cr>", {
+  desc = "Remove date from title under cursor",
+})
+
+-- Search for files in full vault
+vim.keymap.set("n", "<leader>os", function()
+  telescope_builtin.find_files({
+    hidden = false,
+    cwd = "~/Documents/Obsidian Vault",
+  })
+end, {
+  desc = "Search Obsidian Files",
+})
+
+-- Search for grep in full vault
+vim.keymap.set("n", "<leader>oz", function()
+  telescope_builtin.live_grep({
+    hidden = false,
+    cwd = "~/Documents/Obsidian Vault",
+  })
+end, {
+  desc = "Search Obsidian Grep",
+})
+-- for review workflow
+-- move file in current buffer to spaceman1412 folder
+vim.keymap.set("n", "<leader>ok", ":!mv '%:p' /Users/chaileasevn/Documents/Obsidian\\ Vault/spaceman1412<cr>:bd<cr>", {
+  desc = "Move current buffer to spaceman1412 folder",
+})
+-- delete file in current buffer
+vim.keymap.set("n", "<leader>od", function()
+  -- Get the current file path
+  local file = vim.fn.expand("%:p")
+
+  -- Check if the file exists
+  if vim.fn.filereadable(file) == 1 then
+    -- Ask for confirmation
+    local choice = vim.fn.confirm("Are you sure you want to delete this file?", "&Yes\n&No", 2)
+
+    if choice == 1 then -- 1 corresponds to "Yes"
+      -- Delete the file
+      vim.fn.delete(file)
+
+      -- Close the buffer without saving
+      vim.cmd("bdelete!")
+
+      print("File deleted: " .. file)
+    end
+  else
+    print("No file to delete.")
+  end
+end, { noremap = true, silent = true, desc = "Delete current file with confirmation" })
+
+-- Create hub with input
+vim.keymap.set("n", "<leader>oh", function()
+  vim.ui.input({ prompt = "Enter hub name" }, function(input)
+    vim.cmd(":!touch /Users/chaileasevn/Documents/Obsidian\\ Vault/hubs/" .. input .. ".md")
+  end)
+end)
---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,65 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = false,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre ~/Documents/Obsidian Vault/*.md",
+  --   "BufNewFile ~/Documents/Obsidian Vault/*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "/Users/chaileasevn/Documents/Obsidian Vault",
+      },
+    },
+
+    notes_subdir = "inbox",
+    new_notes_location = "notes_subdir",
+    -- see below for full list of options 👇
+    -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+    completion = {
+      -- Set to false to disable completion.
+      nvim_cmp = true,
+      -- Trigger completion at 2 chars.
+      -- min_chars = 2,
+    },
+    ui = {
+      enable = false,
+    },
+    disable_frontmatter = true,
+
+    templates = {
+      subdir = "templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M:%S",
+    },
+    mappings = {
+      -- overrides the 'gf' mapping to work on markdown/wiki links within your vault
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- toggle check-boxes
+      -- ["<leader>ch"] = {
+      --   action = function()
+      --     return require("obsidian").util.toggle_checkbox()
+      --   end,
+      --   opts = { buffer = true },
+      -- },
+    }, -- new_notes_location = "notes_subdir",
+  },
+}
