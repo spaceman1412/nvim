5b4dc642f651b596469fc1c72c3820f2e12181f5
spaceman1412 <spacemannormal@gmail.com>
2024-10-04 15:53:29 +0700

    Config status line by incline

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.winbar = "%=%m %f"
+-- Enable winbar for each buffer
+-- vim.opt.winbar = "%=%m %f"
+
+-- Enable text wrap for responsive size
 vim.o.wrap = true
 
 -- Undercurl
