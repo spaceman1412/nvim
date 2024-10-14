bedbf24554e21726747e38bcc8812dd839c9992a
spaceman1412 <spacemannormal@gmail.com>
2024-10-14 16:16:33 +0700

    Add harpoon and config obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -33,7 +33,7 @@ return {
       -- Set to false to disable completion.
       nvim_cmp = true,
       -- Trigger completion at 2 chars.
-      min_chars = 0,
+      min_chars = 2,
     },
     ui = {
       enable = false,
